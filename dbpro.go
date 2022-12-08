a694a5d7ee14d7d34eea836d945480947a4e0955
Lei Wang <tendant@gmail.com>
2022-12-08 00:14:30 -0800

    change conditions

--- dbpro.go
+++ dbpro.go
@@ -32,9 +32,10 @@ func GenInsertQuery(driverName string, table string, values interface{}) (string
 		if item.Name == "LastModificationTime" {
 			fmt.Println("LastModificationTime:")
 			fmt.Println("field:", field)
+			fmt.Println("field: IsNil", field.IsNil())
 			fmt.Println("kind:", field.Kind())
 		}
-		if (field == reflect.Value{} && field.Kind() == reflect.Struct) {
+		if field.IsNil() && field.Kind() == reflect.Struct {
 			fmt.Println("Empty struct field:", item.Name)
 			continue
 		} else {
